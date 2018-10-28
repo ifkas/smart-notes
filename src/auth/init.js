057b381438ac51773e89a9a4256eb39979c74ce6
Ivo Culic <ivo@tajfa.com>
2018-10-28 16:05:34 +0100

    change to dummy data

--- src/auth/init.js
+++ src/auth/init.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 import firestore from 'firebase/firestore';
 
-// Initialize Firebase
+// Initialize Firebase CHANGE TO YOUR KEYS
 var config = {
-    apiKey: "AIzaSyBZ9yT2ieAB3LJHRK5B5twSYAJepTfjJ3E",
-    authDomain: "quick-notes-31168.firebaseapp.com",
-    databaseURL: "https://quick-notes-31168.firebaseio.com",
-    projectId: "quick-notes-31168",
-    storageBucket: "quick-notes-31168.appspot.com",
-    messagingSenderId: "747931419517"
+    apiKey: "YOUR API KEY HERE",
+    authDomain: "YOUR DOMAIN HERE",
+    databaseURL: "SAME, GET FROM FIREBASE",
+    projectId: "--||--",
+    storageBucket: "--||--",
+    messagingSenderId: "--||--"
   };
 
 const firebaseApp = firebase.initializeApp(config);
